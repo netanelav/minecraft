46e73bf64a899a7c2a2a03e3a59beb830bfaed44
Sapir Malka <sapirm290@gmail.com>
2019-06-17 16:35:43 +0300

    begining of chooseblock function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-var currentTool = "pickaxe"
-function chooseTool(eventObject){
-    currentTool = eventObject.currentTarget.id;
+var currentTool = "pickaxe";
+function chooseTool(eventObject) {
+  currentTool = eventObject.currentTarget.id;
 }
 $(".tool").click(chooseTool);
+
+function chooseBlock(eventObject) {
+    if(eventObject.target.c === ){
+
+    }
+
+}
+$(".block").click(chooseBlock);
