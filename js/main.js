af35018e9df6b790e33d38ea79a586d5be72984a
Netanel <netanel4747@gmail.com>
2019-06-19 11:06:17 +0300

    updated function writing and added hover to block

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let minecraft = {
         rock: { type: "rock", tool: "pickaxe" }
     },
 
-    tools: {},
+    // tools: {},
 
     matrix: [
         [
@@ -527,78 +527,71 @@ let minecraft = {
 
 minecraft.createBoard = function () {
     var gameBoard = $("#gameBoard");
+
     for (var i = 0; i < minecraft.matrix.length; i++) {
         var newRow = document.createElement("div");
         newRow.classList.add("blocks-row");
         newRow.setAttribute("width", "100%");
         gameBoard.append(newRow);
+
         for (var j = 0; j < minecraft.matrix[i].length; j++) {
             var newBlock = document.createElement("span");
             newBlock.classList.add("block");
-            newBlock.setAttribute(
-                "block-type",
-                minecraft.blocks[minecraft.matrix[i][j]].type
-            );
-            newBlock.setAttribute(
-                "tool",
-                minecraft.blocks[minecraft.matrix[i][j]].tool
-            );
-            // .click(minecraft.clickBlock);
+            newBlock.setAttribute("block-type", minecraft.blocks[minecraft.matrix[i][j]].type);
+            newBlock.setAttribute("tool", minecraft.blocks[minecraft.matrix[i][j]].tool);
             newRow.append(newBlock);
         }
     }
 };
 
 minecraft.createBoard();
+
 var currentTool = "pickaxe";
 $("#pickaxe").addClass("blueBorder");
 
-function chooseTool(eventObject) {
+minecraft.chooseTool = function (e) {
     $(`#${currentTool}`).removeClass("blueBorder");
-    currentTool = eventObject.currentTarget.id;
-    console.log(currentTool);
+    currentTool = e.currentTarget.id;
     if (!(currentTool === "stack" && $("#stack").attr("block-type") === "sky"))
         $(`#${currentTool}`).addClass("blueBorder");
-}
+};
+
 $(".tool").click(chooseTool);
 $("#stack").click(chooseTool);
 
-function updateBlockStack(type) {
+minecraft.updateBlockStack = function (type) {
     document.getElementById("stack").setAttribute("block-type", type);
-}
+};
 
-function chooseBlock(eventObject) {
-    let type = eventObject.target.getAttribute("block-type");
-    console.log(eventObject.target.getAttribute("block-type"));
+minecraft.chooseBlock = function (e) {
+    let type = e.target.getAttribute("block-type");
     if (minecraft.blocks[type].tool === currentTool) {
-        console.log("correct tool");
         if (currentTool !== "stack") {
-            eventObject.target.setAttribute("block-type", "sky");
+            e.target.setAttribute("block-type", "sky");
             updateBlockStack(type);
         } else {
-            console.log("tool=stack");
             if (
                 document.getElementById("stack").getAttribute("block-type") !== "sky") {
                 let stackType = document
                     .getElementById("stack")
                     .getAttribute("block-type");
                 if (stackType !== "sky") {
-                    eventObject.target.setAttribute("block-type", stackType);
+                    e.target.setAttribute("block-type", stackType);
                     document.getElementById("stack").setAttribute("block-type", "sky");
                     $("#stack").removeClass("blueBorder");
                 }
             }
         }
     } else {
-        flashRed(currentTool);
+        toolFlashRed(currentTool);
         console.log("incorrect tool");
     }
-}
+};
 
 $(".block").click(chooseBlock);
 document.getElementById("stack").setAttribute("block-type", "sky");
 
-function flashRed(currentTool) {
+minecraft.toolFlashRed = function (currentTool) {
     $(`#${currentTool}`).removeClass("blueBorder");
     $(`#${currentTool}`).addClass("redBorder");
     setTimeout(() => {
