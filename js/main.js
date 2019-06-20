618032d2fe564671d635c28edccc8eceb168ebb1
Netanel <netanel4747@gmail.com>
2019-06-20 16:05:00 +0300

    delete unnecessary code

--- js/main.js
+++ js/main.js
@@ -84,8 +84,6 @@ minecraft.chooseInWorldBlock = function (e) {
     let stackType = $('#stack').attr("block-type");
     if (stackType !== "sky") {
       $(e.target).attr("block-type", stackType);
-      $('#stack').attr("block-type", "sky");
-      $("#stack").removeClass("blueBorder");
     }
     else {
       minecraft.flashRed(minecraft.currentTool);
